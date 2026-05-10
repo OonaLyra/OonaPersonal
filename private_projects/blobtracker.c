81c67619f0a19e8e33da6b223f48a05b35ebb979
OonaLyra <lu.assis.5bc@gmail.com>
2026-05-10 00:27:01 -0300

    Drawing crosses in everything that shines

--- private_projects/blobtracker.c
+++ private_projects/blobtracker.c
@@ -3,7 +3,8 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-/* Ver. 0.0.1 */
+/* Command to compile is: cc blobtracker.c -o blobtracker -lm*/
+/* Ver 0.0.2 */
 /* A structure that i defined up here so it would be */
 /* easier to work with. */
 typedef struct
@@ -19,17 +20,17 @@ int	image_devouring(int	width, int height, unsigned char *data)
 	int	x;
 	int	y;
 	int	index;
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
+	long long sum_x;
+	long long sum_y;
+	long long white_pixels = 0;
+	int	cy;
+	int	cx;
+	int	idx;
 	int	brightness;
-	unsigned char	color;
+	int	i;
 
-	/* to find the pixel the logic is index = (y * width + x) * channels; */
-	/* to change it, the following: */
-	/* data[index] to affect the RED */
-	/* data[index + 1] should affect the GREEN */
-	/* data[index + 2] will affect the BLUE */
+	sum_x = 0;
+	sum_y = 0;
 	y = 0;
 	while (y < height)
 	{
@@ -37,23 +38,53 @@ int	image_devouring(int	width, int height, unsigned char *data)
 		while (x < width)
 		{
 			index = (y * width + x) * 4;
-			r = data[index];
-			g = data[index + 1];
-			b = data[index + 2];
 
 			/* Here we should like, define what is the brightness of a point.*/
-			brightness = (r + g + b) / 3;
-			/* And by the threshold, if it's lower than that value
-			it becomes black, if not, full white. And that's the first step of
-			building a blobtracker to edit those pretty funny images like the
-			one i did today :3*/
-			color = (brightness > 128 ) ?  255:0;
-			data[index] = color;
-			data[index + 1] = color;
-			data[index + 2] = color;
+			brightness = (data[index] + data[index + 1] + data[index + 2]) / 3;
+			
+			/* Let's define that threshold so we'll only take into account
+			things that shine.*/
+			if (brightness > 180)
+			{
+				sum_x += x;
+				sum_y += y;
+				white_pixels++;
+			} else {
+				data[index] = 0;
+				data[index + 1] = 0;
+				data[index + 2] = 0;
+			}
 			x++;
 		}
 		y++;
+	}
+	/* if we find white pixels, we calc the center to draw the UI*/
+	if (white_pixels > 0)
+	{
+		cx = (int)(sum_x / white_pixels);
+		cy = (int)(sum_y / white_pixels);
+	}
+	i = -50;
+	while(i < 50)
+	{
+		/* now we draw a simple crosshair on a blob */
+		/* horizontal line here*/
+		if (cx + i >= 0 && cx + i < width)
+		{
+			idx = (cy * width + (cx + i)) * 4;
+			data[idx] = 255;
+			data[idx + 1] = 0;
+			data[idx + 2] =  0;
+		}
+		/* vertical line here too*/
+		if (cy + i >= 0 && cy + i < height)
+		{
+			idx = ((cy + i) * width + cx) * 4;
+			data[idx] = 255;
+			data[idx + 1] = 0;
+			data[idx + 2] = 0;
+		}
+		i++;
 	}
 	stbi_write_jpg("finished_job.jpg", width, height, 4, data, 100);
 	printf("Imagem devorada e cuspida pela estrela como: finished_job.jpg\n");

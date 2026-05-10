8c86378d2da3204cb1b2b908ca843305c867d5f7
OonaLyra <lu.assis.5bc@gmail.com>
2026-05-10 00:31:17 -0300

    bigger drawing, now its a box. also there's now more to the blob detection

--- private_projects/blobtracker.c
+++ private_projects/blobtracker.c
@@ -3,10 +3,11 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include <stdlib.h>
 /* Command to compile is: cc blobtracker.c -o blobtracker -lm*/
-/* Ver 0.0.2 */
+/* Ver 0.1.0 */
 /* A structure that i defined up here so it would be */
-/* easier to work with. */
+/* easier to work with. And it's finally being used ;3*/
 typedef struct
 {
 	int	id;
@@ -17,77 +18,139 @@ typedef struct
 
 int	image_devouring(int	width, int height, unsigned char *data)
 {
-	int	x;
-	int	y;
-	int	index;
-	long long sum_x;
-	long long sum_y;
-	long long white_pixels = 0;
-	int	cy;
-	int	cx;
 	int	idx;
 	int	brightness;
 	int	i;
-
-	sum_x = 0;
-	sum_y = 0;
+	int	x;
+	int	y;
+	int	pix_idx;
+	int	data_idx;
+	int	b_pixels;
+	int	ky;
+	int	kx;
+	int	ny;
+	int	nx;
+	int	nrs_pix_idx;
+	int cx;
+	int cy;
+	int	b;
+	long long 	b_sum_x;
+	long long	b_sum_y;
+	/* That's kind of a map the pixels already processed so we don't do
+	the same thing over and over*/
+	unsigned char *visited = (unsigned char *)calloc(width * height, 1);
+	Blob blobs[100];
+	int	blob_count = 0;
+	b_sum_x = 0;
+	b_sum_y = 0;
+	b_pixels = 0;
+	/* Threshold built once again*/
+	i = 0;
+	x = 0;
 	y = 0;
-	while (y < height)
+	ky = -20;
+	while (i < width * height)
 	{
-		x = 0;
-		while (x < width)
+		idx = i * 4;
+		brightness = ((data[idx] + data[idx + 1] + data[idx + 2]) / 3);
+		if (brightness > 150)
 		{
-			index = (y * width + x) * 4;
-
-			/* Here we should like, define what is the brightness of a point.*/
-			brightness = (data[index] + data[index + 1] + data[index + 2]) / 3;
-			
-			/* Let's define that threshold so we'll only take into account
-			things that shine.*/
-			if (brightness > 180)
-			{
-				sum_x += x;
-				sum_y += y;
-				white_pixels++;
-			} else {
-				data[index] = 0;
-				data[index + 1] = 0;
-				data[index + 2] = 0;
-			}
-			x++;
-		}
-		y++;
-	}
-	/* if we find white pixels, we calc the center to draw the UI*/
-	if (white_pixels > 0)
-	{
-		cx = (int)(sum_x / white_pixels);
-		cy = (int)(sum_y / white_pixels);
-	}
-	i = -50;
-	while(i < 50)
-	{
-		/* now we draw a simple crosshair on a blob */
-		/* horizontal line here*/
-		if (cx + i >= 0 && cx + i < width)
-		{
-			idx = (cy * width + (cx + i)) * 4;
 			data[idx] = 255;
-			data[idx + 1] = 0;
-			data[idx + 2] =  0;
-		}
-		/* vertical line here too*/
-		if (cy + i >= 0 && cy + i < height)
-		{
-			idx = ((cy + i) * width + cx) * 4;
-			data[idx] = 255;
+			data[idx + 1] = 255;
+			data[idx + 2] = 255;
+		} else {
+			data[idx] = 0;
 			data[idx + 1] = 0;
 			data[idx + 2] = 0;
 		}
 		i++;
 	}
+	// blob detection loop
+	while (y < height)
+	{
+		x = 0;
+		while (x < width)
+		{
+			pix_idx = y * width + x;
+			data_idx = pix_idx * 4;
+			/* Non-visited white pixel*/
+			if (data[data_idx] == 255 && !visited[pix_idx])
+			{
+				if (blob_count >= 100)
+					break;
+				/* Blob meassurement algorythm*/
+				b_sum_x = 0;
+				b_sum_y = 0;
+				b_pixels = 0;
+				ky = -100;
+				while (ky <= 100)
+				{
+					kx = -100;
+					while (kx <= 100)
+					{
+						ny = y + ky;
+						nx = x + kx;
+						if (nx >= 0 && nx < width && ny >= 0 && ny < height)
+						{
+							nrs_pix_idx = ny * width + nx;
+							if (data[nrs_pix_idx * 4] == 255 && !visited[nrs_pix_idx])
+							{
+								visited[nrs_pix_idx] = 1;
+								b_sum_x += nx;
+								b_sum_y += ny;
+								b_pixels++;
+							}
+						}
+						kx++;
+					}
+					ky++;
+				}
+				/* If blob = big enough, store it*/
+				if (b_pixels > 450)
+				{
+					blobs[blob_count].center_x = (int)(b_sum_x / b_pixels);
+					blobs[blob_count].center_y = (int)(b_sum_y / b_pixels);
+					blobs[blob_count].id = blob_count;
+					blob_count++;
+				}
+
+			}
+			x++;
+		}
+		y++;
+	}
+	b = 0;
+    while (b < blob_count)
+    {
+        cx = blobs[b].center_x;
+        cy = blobs[b].center_y;
+        ky = -50; 
+        while (ky <= 50)
+        {
+            kx = -50;
+            while (kx <= 50)
+            {
+                if (ky == -50 || ky == 50 || kx == -50 || kx == 50)
+                {
+                    ny = cy + ky;
+                    nx = cx + kx;
+                    if (nx >= 0 && nx < width && ny >= 0 && ny < height)
+                    {
+                        idx = (ny * width + nx) * 4;
+                        data[idx] = 255;
+                        data[idx + 1] = 0;
+                        data[idx + 2] = 255;
+                    }
+                }
+                kx++;
+            }
+            ky++;
+        }
+        b++;
+    }
 	stbi_write_jpg("finished_job.jpg", width, height, 4, data, 100);
 	printf("Imagem devorada e cuspida pela estrela como: finished_job.jpg\n");
+	free(visited);
 	return (0);
 }
 
